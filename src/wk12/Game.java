9cd0b3d82dd0dd95648f088d9877688c47956b66
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2026-04-02 16:18:45 -0400

    Implement basic game loop with Fighter and Boss

---
+++ src/wk12/Game.java
@@ -0,0 +1,35 @@
+class Game {
+    public static void main(String[] args) {
+        Fighter fighter = new Fighter("Batman", 100, 15, 50);
+        Boss boss = new Boss("Superman", 150, 20, 60);
+
+        Random rand = new Random();
+        int round = 1;
+
+        while (fighter.isAlive() && boss.isAlive()) {
+            System.out.println("---- Round " + round + " ----");
+
+            // Fighter decision: block or attack
+            if (rand.nextInt(100) < 30) {
+                fighter.block();
+            } else {
+                fighter.performAttack(boss);
+            }
+
+            boss.performAttack(fighter);
+
+            // End of round updates
+            fighter.resetBlock();
+            fighter.regenStamina();
+            boss.regenStamina();
+
+            System.out.println(fighter.getStatus());
+            System.out.println(boss.getStatus());
+
+            round++;
+        }
+
+        System.out.println("\nGame Over!");
+         System.out.println((fighter.isAlive() ? fighter.name : boss.name) + " Wins!");
+    }
+}
